eff3cf530036fbbfdc63ab06750afcca2921571e
BFTwChees <64285036+BFTwChees@users.noreply.github.com>
2021-03-27 11:49:22 -0400

    I changed the points and estrusion distance
    I changed it to play around with/understand bowlerstudio

--- extrudePolygon.groovy
+++ extrudePolygon.groovy
@@ -1,9 +1,9 @@
 
 //create an extruded polygon
-CSG polygon = Extrude.points(new Vector3d(0, 0, 40),// This is the  extrusion depth
-                new Vector3d(0,0),// All values after this are the points in the polygon
+CSG polygon = Extrude.points(new Vector3d(0, 0, 20),// This is the  extrusion depth
+                new Vector3d(20,30),// All values after this are the points in the polygon
                 new Vector3d(80,0),// Bottom right corner
-                new Vector3d(60,40),// upper right corner
+                new Vector3d(80,10),// upper right corner
                 new Vector3d(20,40)// upper left corner
         );
 return polygon
